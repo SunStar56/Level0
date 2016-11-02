81ffde5f005dab7c42e64c93dfbf69b11a4c0760
DJA526 <league@WTS13.attlocal.net>
2016-11-01 18:26:24 -0700

    Soundtrack To Life
    Unfinished
    
    Started 11/1/16

--- src/CrazyCatLady.java
+++ src/CrazyCatLady.java
@@ -8,15 +8,13 @@ public class CrazyCatLady {
 		String Cats = JOptionPane.showInputDialog("How many cats do you have? :3");
 		int cat = Integer.parseInt(Cats);
 		if (cat > 3) {
-			JOptionPane.showMessageDialog(null, "You are a crazy cat lady.  No other words cat describe you.");
+			JOptionPane.showMessageDialog(null, "You are a crazy cat lady.  No other words can describe you.");
 
 		} else if (cat < 3 && cat > 0) {
 			playVideo("https://www.youtube.com/watch?v=3EIbWjkimAs");
+		} else {
+			playVideo("https://www.youtube.com/watch?v=SKRgktzRvZ0");
 		}
-
-		// 4. If they have 3 or less, call the method below to show them a cat video
-
-		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
 
 	}
 
