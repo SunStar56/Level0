fef25db082ef982492fe1a007d835c71475592f6
SunStar56 <league@WorthyMini.attlocal.net>
2016-12-06 18:30:21 -0800

    ZorkYay
    More Zork Saving

--- src/ZorkYay.java
+++ src/ZorkYay.java
@@ -1,4 +1,9 @@
+import javax.swing.JOptionPane;
 
 public class ZorkYay {
+	public static void main(String[] args) {
 
+		JOptionPane.showMessageDialog(null,
+				"Greetings, traveller.  You are in the town of Nesretep.  EAST - Trade Center NORTH - Inn WEST - Town Gates SOUTH - Cafe");
+	}
 }
